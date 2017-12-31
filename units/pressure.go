61fa005822338d8a2080c64ba887d259c93f3288
Eric Barkie <ebarkie@users.noreply.github.com>
2017-12-31 13:58:01 -0500

    Add Inches to Millibars

---
+++ units/pressure.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2016-2017 Eric Barkie. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE file.
+
+package units
+
+// Mb converts Inches to Millibars.
+func Mb(i float64) float64 {
+	return i * 33.8637526
+}
---
+++ units/pressure_test.go
@@ -0,0 +1,15 @@
+// Copyright 2016-2017 Eric Barkie. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE file.
+
+package units
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMb(t *testing.T) {
+	assert.Equal(t, 846.593815, Mb(25.0), "Inches to Millibars")
+}
